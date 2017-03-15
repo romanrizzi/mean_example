9540f544ac98098dcc3d5f18a2f78e9fd8670394
Roman Rizzi <rizziromanalejandro@gmail.com>
2017-03-15 19:40:34 -0300

    Basic route

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 import express from 'express'
 
 const port = 3001
-express().listen(port, () => console.log(`Server running on ${port}`))
+const app = express()
+
+app.listen(port, () => console.log(`Server running on ${port}`))
+
+app.get('/greet', (req, res) => {
+  res.status(200).send(`Hola ${req.query.nombre}`)
+})
