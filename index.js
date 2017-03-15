4b041a055bfdea098980f53d1480a2bba68cffa5
Roman Rizzi <rizziromanalejandro@gmail.com>
2017-03-15 20:05:06 -0300

    Save and read noticias

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 import express from 'express'
+import bodyParserFactory from 'body-parser'
 
 const port = 3001
 const app = express()
-
+app.use(bodyParserFactory.json())
 app.listen(port, () => console.log(`Server running on ${port}`))
 
-app.get('/greet', (req, res) => {
-  res.status(200).send(`Hola ${req.query.nombre}`)
+let noticias = []
+
+app.get('/noticias', (req, res) => {
+  res.json(noticias)
+})
+
+app.post('/noticias', (req, res) => {
+  const noticia = req.body
+  noticias.push(noticia)
+  res.sendStatus(200)
 })
