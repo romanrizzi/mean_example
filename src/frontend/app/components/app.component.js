8f1d3ec8b3178eafba9ef484ec5c95c919cb9179
Roman Rizzi <rizziromanalejandro@gmail.com>
2017-03-27 21:09:14 -0300

    Extraer estado a un servicio

--- src/frontend/app/components/app.component.js
+++ src/frontend/app/components/app.component.js
@@ -1,19 +1,19 @@
-import { Component } from '@angular/core'
+import { Component } from '@angular/core';
+import PostService from '../services/post.service';
 
 @Component({
   selector: 'app-view',
   template: `<h1>Bienvenidos a {{name}}</h1>
-             <post *ngFor="let item of posts" [data]=item></post>
-             <newPost></newPost>
-            `
+            <post *ngFor="let item of posts" [data]="item"></post>
+
+            <newPost></newPost>`,
+  providers: [ PostService ]
 })
 export default class AppComponent {
-  constructor() {
-    this.name = 'Noticias UNQ'
-    this.posts = [
-      { title: 'Noticia 1', content: 'Contenido 1', author: 'Claudio', upvotes: 0 },
-      { title: 'Noticia 2', content: 'Contenido 2', author: 'Claudio', upvotes: 0 },
-      { title: 'Noticia 3', content: 'Contenido 3', author: 'Claudio', upvotes: 0 }
-    ]
+  constructor(postService) {
+    this.name = 'Noticias UNQ',
+    this.posts = postService.posts;
   }
 }
+
+AppComponent.parameters = [PostService]
